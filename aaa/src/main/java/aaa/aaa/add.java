e7068ccf89f30ae0ff79eab70394033261f25c54
ex106 <ex106@win2k18.local>
2023-12-21 09:35:59 +0530

    add and sub

--- aaa/src/main/java/aaa/aaa/add.java
+++ aaa/src/main/java/aaa/aaa/add.java
@@ -1,5 +1,10 @@
 package aaa.aaa;
 
 public class add {
-
+	public static void main(string[] args){
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println("Addition= "+sum);
+	}
 }
--- aaa/src/main/java/aaa/aaa/sub.java
+++ aaa/src/main/java/aaa/aaa/sub.java
@@ -1,5 +1,11 @@
 package aaa.aaa;
 
 public class sub {
+	 public static void main(string[] args){
+                int a = 10;
+                int b = 20;
+                int sub = a-b;
+                System.out.println("Addition= "+sub);
+        }
 
 }
