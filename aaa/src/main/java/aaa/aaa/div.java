2696177df6807b777efdd7a80a4addffc8ceaec6
root <root@ip-172-31-30-202.ap-southeast-1.compute.internal>
2023-12-21 04:18:15 +0000

    mul and div

--- aaa/src/main/java/aaa/aaa/div.java
+++ aaa/src/main/java/aaa/aaa/div.java
@@ -1,5 +1,11 @@
 package aaa.aaa;
 
 public class div {
+	public static void main(string[] args){
+                int a = 10;
+                int b = 20;
+                int div = a/b;
+                System.out.println("Division= "+div);
+        }
 
 }
--- aaa/src/main/java/aaa/aaa/mul.java
+++ aaa/src/main/java/aaa/aaa/mul.java
@@ -1,5 +1,10 @@
 package aaa.aaa;
 
 public class mul {
-
+	public static void main(string[] args){
+		int a = 10;
+		int b = 20;
+		int mul = a*b;
+		System.out.println("Multiplication= "+mul);
+	}
 }
